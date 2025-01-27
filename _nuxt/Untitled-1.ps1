3c22dc6cae5b3da8974afc0c63e9103471838434
matthewkyleherren <99541432+matthewkyleherren@users.noreply.github.com>
2025-01-27 11:06:04 +0100

    uzfuz

---
+++ _nuxt/Untitled-1.ps1
@@ -0,0 +1,149 @@
+wget -nH -x https://www.jeton.com/_nuxt/JetonWalkthrough.D2USk2ZX.css;
+wget -nH -x https://www.jeton.com/_nuxt/CurrencyExchangeCalculator.CaNBs1PA.css;
+wget -nH -x https://www.jeton.com/_nuxt/ExtraBoldHero.RuPJ55iK.css;
+wget -nH -x https://www.jeton.com/_nuxt/ClientTestimonials.bOetmxe9.css;
+wget -nH -x https://www.jeton.com/_nuxt/MediaHero.CAJbmgj3.css;
+wget -nH -x https://www.jeton.com/_nuxt/HeroIntroduction.Zz5M2fap.css;
+wget -nH -x https://www.jeton.com/_nuxt/CommonHeader.DFxrkEKr.css;
+wget -nH -x https://www.jeton.com/_nuxt/Footer.QUSiLTRD.css
+
+wget -nH -x https://www.jeton.com/_nuxt/PageTransition.CReKMGQ4.css
+
+
+wget -nH -x https://www.jeton.com/icons/round-world-flags/albania.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/bosnia%20and%20herzegovina.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/ireland.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/hungary.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/croatia.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/san%20marino.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/cyprus.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/czech%20republic.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/india.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/maldives.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/montenegro.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/vatican%20city.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/france.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/mauritius.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/mauritania.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/pakistan.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/ukraine.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/mexico.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/oman.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/barbados.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/madagascar.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/greece.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/liberia.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/malawi.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/bolivia.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/belize.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/bahamas.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/mali.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/morocco.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/equatorial%20guinea.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/ghana.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/malta.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/singapore.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/panama.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/samoa.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/seychelles.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/slovakia.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/sierra%20leone.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/bahrain.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/bulgaria.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/philippines.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/malaysia.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/niger.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/brazil.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/bangladesh.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/laos.svg;
+wget -nH -x https://www.jeton.com/icons/round-world-flags/lesotho.svg
+
+wget -nH -x https://www.jeton.com/icons/round-world-flags/
+
+
+wget -nH -x https://www.jeton.com/icons/currencies/currency-rates;
+[Error] SyntaxError: The string did not match the expected pattern.
+	(anonymous function) (BthkyGhE.js:1:1104)
+wget -nH -x https://www.jeton.com/icons/currencies/AUD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/CLP.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/BHD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/BGN.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/BRL.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/CAD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/AED.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/CZK.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/DKK.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/GHS.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/HUF.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/COP.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/HKD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/IDR.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/KWD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/%EF%BF%A5.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/INR.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/ISK.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/CRC.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/MYR.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/JOD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/CHF.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/TND.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/RSD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/QAR.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/MXN.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/USD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/SAR.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/PEN.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/NZD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/MAD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/NOK.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/ZAR.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/SGD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/PLN.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/RON.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/THB.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/TWD.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/OMR.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/SEK.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/KRW.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/NGN.svg;
+wget -nH -x https://www.jeton.com/icons/currencies/VND.svg;
+
+wget -nH -x https://www.jeton.com/icons/currencies/
+
+https%3A%2F%2Fjeton-gilt.vercel.app%2Ficons%2Fcurrencies%2F%EF%BF%A5.svg
+
+wget -nH -x https://www.jeton.com/api/currency-rates
+
+curl -O /rivs/23a3c11ba226d4bb7e916918723d814288b77265.riv;
+curl -O /rivs/6442a9110636ba0da2e6c0c731d92457d7e928cc.riv;
+curl -O /rivs/01ab41d2c3f10a4db55f02c855b37106ae2d2568.riv;
+curl -O /rivs/95cea0426464659130966516e85464603be1e091.riv;
+curl -O /rivs/8d69d40e5b4bce75fcc798d223029db8a1ccdeab.riv
+
+curl -O https://www.jeton.com/rivs/f6443202df7a64be83ae43d813286057280a0561.riv
+
+wget -nH -x "https://www.jeton.com/cookie-policy/_payload.json?154a82f9-866e-432c-a8ec-c788cd590612"
+
+
+wget -nH -x https://www.jeton.com/_nuxt/ParallaxFeatureShowcase.aOuEe8hG.css;
+wget -nH -x https://www.jeton.com/_nuxt/ImageBundleShowcase.DRBCTfsc.css;
+wget -nH -x https://www.jeton.com/_nuxt/JetonCardFeatures.Dst9jE0m.css;
+wget -nH -x https://www.jeton.com/_nuxt/JetonCardWallets.DxK2aECw.css;
+wget -nH -x https://www.jeton.com/_nuxt/JetonCardHeroIntroduction.2vXrXZFl.css;
+wget -nH -x https://www.jeton.com/_nuxt/TabbedAccordionList.CMtg0kue.css
+
+wget -nH -x https://www.jeton.com/_nuxt/FilteredRichTextContent.Boma9lal.css;
+wget -nH -x https://www.jeton.com/_nuxt/AccountPricing.BAmQ9G-J.css;
+
+
+https://jeton-kw0q5ugqh-team-kazi.vercel.app/_nuxt/CertificationsList.CGmWJFZg.css
+
+wget -nH -x https://www.jeton.com/_nuxt/JetonMission.DbZle50y.css;
+wget -nH -x https://www.jeton.com/_nuxt/JetonValues.fYrzZGAF.css;
+wget -nH -x https://www.jeton.com/_nuxt/CertificationsList.CGmWJFZg.css;
+wget -nH -x https://www.jeton.com/_nuxt/JetonProduct.K4SB62u5.css;
+wget -nH -x https://www.jeton.com/_nuxt/JetonProduct.CemSTqFS.css;
+wget -nH -x https://www.jeton.com/_nuxt/ArticlesList.CbGW7EJL.css;
+wget -nH -x https://www.jeton.com/_nuxt/PartnershipsList.DjmG-FMm.css
+wget -nH -x https://www.jeton.com/_nuxt/BrandKit.B6Qzqp_a.css;
+wget -nH -x https://www.jeton.com/_nuxt/BrandKit.D34iGFHu.css
